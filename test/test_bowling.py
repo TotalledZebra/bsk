fe5b9edb398c7e84071a4df59af0a897032138a9
TotalledZebra <Holappa57@gmail.com>
2024-10-28 16:51:20 +0200

    Add logic to handle out of bounds frame access

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -16,3 +16,9 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(frame)
 
         self.assertEqual(frame, game.get_frame_at(0))
+
+    def test_raises_error_when_accessing_out_of_bounds(self):
+
+        game = BowlingGame()
+
+        self.assertRaises(BowlingError, game.get_frame_at, 0)
